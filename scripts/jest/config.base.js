502127624069bc6bb5d19d34bdafc68bb445c8b4
NE-SmallTown <ne_smalltown@163.com>
2018-09-08 11:01:08 +0800

    ss

--- scripts/jest/config.base.js
+++ scripts/jest/config.base.js
@@ -15,10 +15,9 @@ module.exports = {
   setupFiles: [require.resolve('./setupEnvironment.js')],
   setupFilesAfterEnv: [require.resolve('./setupTests.js')],
   // Only include files directly in __tests__, not in nested folders.
-  testRegex: '/__tests__/[^/]*(\\.js|\\.coffee|[^d]\\.ts)$',
-  // testRegex: '/__tests__/ReactSuspense-test.internal(\\.js|\\.coffee|[^d]\\.ts)$',
-  // testRegex: '/__tests__/ReactUpdates-test(\\.js|\\.coffee|[^d]\\.ts)$',
-  // testNamePattern: 'should queue nested updates',
+  // testRegex: '/__tests__/[^/]*(\\.js|\\.coffee|[^d]\\.ts)$',
+  testRegex: '/__tests__/ReactDOMFiberAsync-test.internal(\\.js|\\.coffee|[^d]\\.ts)$',
+  testNamePattern: 'flushSync throws if already performing work',
   moduleFileExtensions: ['js', 'json', 'node', 'coffee', 'ts'],
   rootDir: process.cwd(),
   roots: ['<rootDir>/packages', '<rootDir>/scripts'],
